4c2247401c41b41422166ead76f99e52ac001e40
TheCanadians <vschnoor@mc-kiel.de>
2020-07-06 13:01:43 +0200

    Waypoints platziert

--- FS_SS2020/Assets/Scripts/DummyKeyboardController.cs
+++ FS_SS2020/Assets/Scripts/DummyKeyboardController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
 
 public class DummyKeyboardController : MonoBehaviour
 {
@@ -9,11 +11,24 @@ public class DummyKeyboardController : MonoBehaviour
     [SerializeField] private float verSpeed = 10f;
     [SerializeField] private float rotSpeed = 3f;
 
+    [SerializeField] private Volume volume;
+    private MotionBlur mb;
+
     // Start is called before the first frame update
     void Start()
     {
         if (wpMan == null)
             Debug.Log("WaypointManager is NULL");
+
+        MotionBlur tempMB;
+
+        if (volume != null)
+        {
+            if (volume.profile.TryGet<MotionBlur>(out tempMB))
+            {
+                mb = tempMB;
+            }
+        }
     }
 
     // Update is called once per frame
@@ -29,6 +44,15 @@ public class DummyKeyboardController : MonoBehaviour
     {
         float ver = Input.GetAxis("Vertical");
         float rot = Input.GetAxis("Horizontal");
+
+        if (rot != 0)
+        {
+            mb.intensity.Override(7f);
+        }
+        else
+        {
+            mb.intensity.Override(0f);
+        }
 
         float translation = ver * verSpeed * Time.deltaTime;
         float rotation = rot * rotSpeed * Time.deltaTime;
